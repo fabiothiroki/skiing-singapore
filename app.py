69b267420d012e973215b83e75267dcb48be2a8e
fabiothiroki <fabiothiroki@gmail.com>
2015-08-09 16:12:55 -0300

    graph object

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
 import sys
+import pprint
 
-with open(sys.argv[1], 'r') as my_file:
-	str_first_line = my_file.readline().strip()
-	arr_first_line = str_first_line.split(' ', 1 )
+def read_input():
+
+	with open(sys.argv[1], 'r') as my_file:
+		
+		inputText = (my_file.read())
+
+		return inputText
+
+def populateGraph(inputText):
+	graphDict = {}
+	pp = pprint.PrettyPrinter(indent=4)
+
+	txt_lines = inputText.splitlines()
+	arr_first_line = txt_lines[0].split(' ', 1 )
 
 	num_lines = arr_first_line[0]
-	num_columns =  arr_first_line[1]
+	num_columns = arr_first_line[1]
 
-	print num_lines, num_columns
+	for line in range(1, int(num_lines)):
+		current_line = txt_lines[line].split(' ', int(num_columns) - 1 )
+		
+		for column in range (0, int(num_columns)):
+			graphDict[ str(line) + ' ' + str(column)  ] = set()
 
-	inputText = (my_file.read())
+	pp.pprint(graphDict)
+
+
+inputText = read_input()
+populateGraph(inputText)
 
